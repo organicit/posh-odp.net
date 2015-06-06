e1ee9e89a63486d9762beb8630ea467d86877299
Eric Courville <edhsystems@gmail.com>
2015-06-06 08:08:18 +0000

    added demo1.ps1

---
+++ demo1.ps1
@@ -0,0 +1,3 @@
+﻿Add-Type -path "C:\oracle\ODP.NET12c\odp.net\managed\common\Oracle.ManagedDataAccess.dll"
+
+$con = New-Object Oracle.ManagedDataAccess.Client.OracleConnection("User Id=scott;Password=lsdjkmL!k34;Data Source=orgitorademo.cloudapp.net/poshdemo")
